9170b6b3b22953f2c025d97c1c42eafba3322787
RandomRaine <codezeraven@gmail.com>
2022-12-07 22:58:10 -0800

    Day01 part 2

--- Day01/Day01.js
+++ Day01/Day01.js
@@ -1,7 +1,7 @@
 const file = require("fs").readFileSync("input.txt", "utf8");
 const line = file.split("\n\n");
 
-const elfCalories = [];
+const Erufu = [];
 let currentElf = 1;
 const snacks = [
   "Candy Cane",
@@ -25,13 +25,13 @@ const snacks = [
 ];
 
 for (let i = 0; i < line.length; i++) {
-  elfCalories.push({ name: `elf ${currentElf}`, snacks: [] });
+  Erufu.push({ name: `elf ${currentElf}`, snacks: [], totalCalories: 0 });
   const calories = line[i].split("\n");
 
   for (let j = 0; j < calories.length; j++) {
     var snack = snacks[Math.floor(Math.random() * snacks.length)];
 
-    elfCalories[i].snacks.push({
+    Erufu[i].snacks.push({
       name: snack,
       calories: parseInt(calories[j]),
     });
@@ -42,22 +42,35 @@ for (let i = 0; i < line.length; i++) {
 var maxCalories = 0;
 var elfWithMaxCalories = null;
 
-for (var i = 0; i < elfCalories.length; i++) {
-  var elf = elfCalories[i];
+for (var i = 0; i < Erufu.length; i++) {
+  var elf = Erufu[i];
   var elfTotalCalories = 0;
   for (var j = 0; j < elf.snacks.length; j++) {
     elfTotalCalories += elf.snacks[j].calories;
   }
+  Erufu[i].totalCalories = elfTotalCalories;
   if (elfTotalCalories > maxCalories) {
     maxCalories = elfTotalCalories;
     elfWithMaxCalories = elf;
   }
 }
-console.log(elfCalories[0]);
+console.log(Erufu[0]);
 console.log(
   "The Elf carrying the most calories is " +
     elfWithMaxCalories.name +
     " and is carrying " +
     maxCalories +
     " calories."
+);
+
+var topThree = Erufu.sort(function (a, b) {
+  return b.totalCalories - a.totalCalories;
+}).slice(0, 3);
+console.log("ZR topThree", topThree);
+var totalCalories = topThree.reduce(function (sum, elf) {
+  return sum + elf.totalCalories;
+}, 0);
+
+console.log(
+  "How many Calories are those Elves carrying in total? " + totalCalories
 );
